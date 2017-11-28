ef90a18e586906200066cf19bb50a05698e0b04b
blaze515 <dunnj515@gmail.com>
2017-11-27 23:33:04 -0600

    Add multiple cats

--- js/main.js
+++ js/main.js
@@ -1,17 +1,62 @@
+var initialCats = [
+    {
+        name: 'Mike',
+        imgSrc: 'img/mike.jpg',
+        clickCount: 0,
+        nicknames: [{nickname: 'Mikey'}, {nickname: 'Michael'}, {nickname: 'Dungeon Master'}]
+    },
+    {
+        name: 'Jane',
+        imgSrc: 'img/eleven.jpg',
+        clickCount: 0,
+        nicknames: [{nickname: 'Eleven'}, {nickname: 'Freak'}]
+    },
+    {
+        name: 'Nancy',
+        imgSrc: 'img/nancy.jpg',
+        clickCount: 0,
+        nicknames: [{nickname: 'Princess Nancy'}]
+    },
+    {
+        name: 'Steve',
+        imgSrc: 'img/steve.jpg',
+        clickCount: 0,
+        nicknames: [{nickname: 'King Steve'}]
+    },
+    {
+        name: 'Jim Hopper',
+        imgSrc: 'img/hopper.jpg',
+        clickCount: 0,
+        nicknames: [{nickname: 'Hopper'}, {nickname: 'Hop'}, {nickname: 'Chief'}]
+
+    },
+    {
+        name: 'Demagorgan',
+        imgSrc: 'img/demagorgan.jpg',
+        clickCount: 0,
+        nicknames: [{nickname: 'The Monster'}, {nickname: 'The Man with No Face'}]
+
+    }
+];
+
 var ViewModel = function() {
     var self = this;
+
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(catItem){
+        self.catList.push(new Cat(catItem));
+    });
+
+    this.currentCat = ko.observable(this.catList()[0]);
 
     this.incrementCounter = function() {
         self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
 
-    this.currentCat = ko.observable(new Cat({
-        clickCount: 0,
-        name: 'Mike',
-        imgSrc: 'img/mike.jpg',
-        imgAttribution: 'http://www.kedi.gen.tr/images/yavru-kedi.jpg',
-        nicknames: [{nickname: 'Mikey'}, {nickname: 'Michael'}, {nickname: 'Dungeon Master'}]
-    }));
+    this.changeCat =  function (clickedCat) {
+        self.currentCat(clickedCat);
+    };
 };
 
 var Cat = function (data) {
